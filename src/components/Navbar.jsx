4ec03981070e3ee5eb268b77f731aced5a0b2286
Alan Gambacorta <Alangambacorta7@gmail.com>
2026-07-05 17:26:18 -0300

    Logo navbar mas grande h-36

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar({ tab, setTab }) {
   return (
     <header className="bg-navy-800 border-b border-navy-700 sticky top-0 z-10">
       <div className="w-full flex justify-center py-3 border-b border-navy-700/50">
-        <img src="/logo.png" alt="BitácoraAR" className="h-20 w-auto" />
+        <img src="/logo.png" alt="BitácoraAR" className="h-36 w-auto" />
       </div>
       <div className="max-w-6xl mx-auto px-4 py-2">
         <nav className="flex gap-1 overflow-x-auto scrollbar-none justify-center">
